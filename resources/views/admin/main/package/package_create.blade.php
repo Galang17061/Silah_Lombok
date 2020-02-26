bf1d8658a42689335b41ce7f6eefb11c710d832f
AzrieL/Muttq <denyprasetyo41@gmail.com>
2020-02-27 03:48:49 +0700

    12123

--- resources/views/admin/main/package/package_create.blade.php
+++ resources/views/admin/main/package/package_create.blade.php
@@ -128,15 +128,7 @@
                                     </div>
                                     {{-- TAB KE 4 --}}
                                     <div class="tab-pane active" id="price_table" role="tabpanel">
-                                        <p class="mb-0">
-                                            Trust fund seitan letterpress, keytar raw denim keffiyeh etsy
-                                            art party before they sold out master cleanse gluten-free squid
-                                            scenester freegan cosby sweater. Fanny pack portland seitan DIY,
-                                            art party locavore wolf cliche high life echo park Austin. Cred
-                                            vinyl keffiyeh DIY salvia PBR, banh mi before they sold out
-                                            farm-to-table VHS viral locavore cosby sweater. Lomo wolf viral,
-                                            mustache.
-                                        </p>
+                                        
                                     </div>
                                 </div>
                               </form>
